b123d18068112a248691f2004da3e31b1b72ede8
Jay <djautoofficial@gmail.com>
2018-09-25 20:39:56 +0200

    0.1 version

---
+++ main.py
@@ -0,0 +1,28 @@
+print ("  Population Predictor 0.1");
+print ("");
+print ("  Based On Google's Statistics");
+print ("");
+
+ireland_stat = 0.035;
+ireland_2016 = 4.726;
+year_difference = 0;
+result = 0;
+cal_year = 2016;
+
+country_input = input("  Choose Country: ");
+year_input = input("  Choose Year: ");
+
+if (country_input == "ireland"):
+  print ("");
+  year_difference = int(year_input) - int(cal_year)
+  print ("  In " + str(year_difference) + " years there will be");
+  result = ireland_2016 + ireland_stat * year_difference
+  print ("  " + str(result) + " million people in Ireland");
+  print ("");
+else:
+  print ("");
+  print ("  Unsupported Country!");
+
+print ("");  
+print ("  Keep in mind that these results aren't very accurate, accuracy decreases with each year!");
+print ("");
